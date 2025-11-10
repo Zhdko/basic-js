3b9e1bd4bf554b2bb01287cf3e4a677af2fa4dc8
Alexandr Zhdanko <lex.zhdanko@gmail.com>
2025-11-10 22:22:07 +0400

    feat: implement vigenere-chipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,15 +19,58 @@ const { NotImplementedError } = require('../lib');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  *
  */
+
 class VigenereCipheringMachine {
-  encrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
   }
 
-  decrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!');
+    }
+    return this.process(String(message).toUpperCase(), String(key).toUpperCase(), true);
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (encryptedMessage === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!');
+    }
+    return this.process(String(encryptedMessage).toUpperCase(), String(key).toUpperCase(), false);
+  }
+
+  process(msg, key, isEncrypt) {
+    const A = 'A'.charCodeAt(0);
+    const isLetter = (ch) => ch >= 'A' && ch <= 'Z';
+
+    const keyIdx = [];
+    for (let i = 0; i < key.length; i++) {
+      const ch = key[i];
+      if (isLetter(ch)) keyIdx.push(ch.charCodeAt(0) - A);
+    }
+
+    let ki = 0;
+    const out = [];
+
+    for (let i = 0; i < msg.length; i++) {
+      const ch = msg[i];
+      if (!isLetter(ch)) {
+        out.push(ch);
+        continue;
+      }
+      const m = ch.charCodeAt(0) - A;
+      const k = keyIdx[ki % keyIdx.length];
+      ki++;
+
+      const code = isEncrypt
+        ? (m + k) % 26
+        : (m - k + 26) % 26;
+
+      out.push(String.fromCharCode(A + code));
+    }
+
+    if (!this.isDirect) out.reverse();
+    return out.join('');
   }
 }
 
