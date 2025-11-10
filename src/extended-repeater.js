ea1591c90e4aa6c1801074014a5332220ba8e120
Alexandr Zhdanko <lex.zhdanko@gmail.com>
2025-11-10 20:23:36 +0400

    feat: implement extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -16,9 +16,27 @@ const { NotImplementedError } = require('../lib');
  *
  */
 
-function repeater(/* str, options */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function repeater(str, options) {
+ const base = String(str);
+
+  const repeatTimes = options.repeatTimes ?? 1;
+  const separator = options.separator ?? '+';
+
+  const hasAddition = Object.prototype.hasOwnProperty.call(options, 'addition');
+  const addition = hasAddition ? String(options.addition) : '';
+
+  const additionRepeatTimes = options.additionRepeatTimes ?? 1;
+  const additionSeparator = options.additionSeparator ?? '|';
+
+  const additionChunk = additionRepeatTimes > 0
+    ? Array(additionRepeatTimes).fill(addition).join(additionSeparator)
+    : '';
+
+  const unit = base + additionChunk;
+
+  return repeatTimes > 0
+    ? Array(repeatTimes).fill(unit).join(separator)
+    : '';
 }
 
 module.exports = {
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,19 @@ const { NotImplementedError } = require('../lib');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function sortByHeight(arr) {
+  const res = [...arr];
+
+  const heights = res.filter(x => x !== -1).sort((a, b) => a - b);
+
+  let i = 0;
+  for (let idx = 0; idx < res.length; idx++) {
+    if (res[idx] !== -1) {
+      res[idx] = heights[i++];
+    }
+  }
+
+  return res;
 }
 
 module.exports = {
